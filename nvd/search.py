0279cc14bbbdbbe35ab6b0dc5566a7820398307c
ck9 <0926cookie@gmail.com>
2021-12-19 04:29:47 +0900

    add NVD

---
+++ nvd/search.py
@@ -0,0 +1,21 @@
+import requests, json
+
+from pprint import pprint
+
+sign = input('Enter Signature : ')
+url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
+payload = {"keyword":sign}
+res_json = requests.get(url, params=payload)
+res = json.loads(res_json.text)
+
+if res["resultsPerPage"]!=0:
+  arr = {}
+  for item in res["result"]["CVE_Items"]:
+    cve = item["cve"]["CVE_data_meta"]["ID"]
+    arr[cve] = {'cve': cve, 'url':'https://nvd.nist.gov/vuln/detail/'+cve}
+  print(arr)
+else:
+  print("Not Found")
+
+  
+  
