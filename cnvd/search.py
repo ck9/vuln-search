047484a3a64ee1e38b351b48b3f1cfedfb49c006
ck9 <0926cookie@gmail.com>
2022-01-12 17:16:13 +0900

    change select columns

--- cnvd/search.py
+++ cnvd/search.py
@@ -5,7 +5,7 @@ def search(sign):
   sign = re.sub('^/+', '', sign)
   conn = sqlite3.connect('exploits.db')
   c = conn.cursor()
-  c.execute("SELECT * FROM exploits WHERE description like ?", ('%'+sign+'%',))
+  c.execute("SELECT id,url FROM exploits WHERE description like ?", ('%'+sign+'%',))
   res = c.fetchall()
   conn.commit()
   conn.close()
