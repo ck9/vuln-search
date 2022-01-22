3a2c1190bdb8843c570c6ec7e0cf11c5615f79c2
ck9 <0926cookie@gmail.com>
2022-01-22 11:10:28 +0900

    add fuzzysearch

--- cnvd/search.py
+++ cnvd/search.py
@@ -1,5 +1,6 @@
 import sqlite3, re
 from pprint import pprint
+import Levenshtein
 
 def search(sign):
   sign = re.sub('^/+', '', sign)
@@ -14,4 +15,49 @@ def search(sign):
   for item in res:
     arr[item[0]] = item[1]
   
+  return arr
+
+def search_path(sign):
+  sign = re.sub('^/+', '', sign)
+  conn = sqlite3.connect('exploits.db')
+  c = conn.cursor()
+  c.execute("SELECT id,url FROM exploits WHERE path like ?", ('%'+sign+'%',))
+  res = c.fetchall()
+  conn.commit()
+  conn.close()
+  
+  arr = {}
+  for item in res:
+    arr[item[0]] = item[1]
+  
+  return arr
+
+
+def search_path_exact(sign):
+  sign = re.sub('^/+', '', sign)
+  conn = sqlite3.connect('exploits.db')
+  c = conn.cursor()
+  c.execute("SELECT id,url FROM exploits WHERE path like ?", [sign])
+  res = c.fetchall()
+  conn.commit()
+  conn.close()
+  
+  arr = {}
+  for item in res:
+    arr[item[0]] = item[1]
+  
+  return arr
+
+def fuzzy_search(sign, score=0.9):
+  sign = re.sub('^/+', '', sign)
+  conn = sqlite3.connect('exploits.db')
+  c = conn.cursor()
+  c.execute("SELECT id,url,path FROM exploits")
+  res = c.fetchall()
+  conn.commit()
+  conn.close()
+  arr = {}
+  for item in res:
+    if(Levenshtein.jaro_winkler(sign, item[2]) > score):
+      arr[item[0]] = item[1]
   return arr
