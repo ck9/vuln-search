6b046b7510b88a1919aca1cddd5d8e866e419748
ck9 <0926cookie@gmail.com>
2022-04-07 11:40:53 +0900

    modify directory structure

--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-from functions import get_option
+from src.functions import get_option
 from pprint import pprint
 
 opts = get_option()
@@ -10,17 +10,17 @@ if(not opts.querySearch):
   sign = sign.split("?")[0]
 
 print("nvd")
-from nvd.search import search as search_nvd
+from src.nvd.search import search as search_nvd
 pprint(search_nvd(sign))
 
 print("exploitdb")
-from exploitdb.search import search as search_edb
+from src.exploitdb.search import search as search_edb
 pprint(search_edb(sign))
 
 print("vulners")
-from vulners_.search import search as search_vulners
+from src.vulners_.search import search as search_vulners
 pprint(search_vulners(sign))
 
 print("cnvd")
-from cnvd.search import search as search_cnvd
+from src.cnvd.search import search as search_cnvd
 pprint(search_cnvd(sign))
